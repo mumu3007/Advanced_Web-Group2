c70b36ed400ae8323dd7e05f71da0fc6067e064a
jay <142081719+JayPornananratKan@users.noreply.github.com>
2024-09-04 17:14:53 +0700

    🎨 install cors env

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
+const dotenv = require("dotenv");
+
+const cors = require("cors")
+
 const expressFunction = require('express');
 const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://thanawat352:1234@cafeboard.cygfo.mongodb.net/?retryWrites=true&w=majority&appName=cafeboard";
 
-const client = new MongoClient(uri, {
+dotenv.config()
+
+const { ATLAS_URL } = process.env;
+
+const client = new MongoClient(ATLAS_URL, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -18,32 +25,17 @@ const app = expressFunction();
 async function connectToMongoDB() {
   try {
     await client.connect();
+    app.use(cors())
     console.log("Connected to MongoDB!");
+    app.use('/menu',getAllMenu)
+    app.use('/boardgame',getAllBoardgame)
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
   }
 }
 
-// Define a GET route to fetch data from the students collection
-app.get('/students', async (req, res) => {
-  try {
-    const database = client.db('cafeboardgame');
-    const collection = database.collection('students');
-    
-    // Fetch all documents from the students collection
-    const students = await collection.find({}).toArray();
-    
-    // Send the retrieved documents as a response
-    res.status(200).json(students);
-  } catch (error) {
-    console.error("Error fetching students:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-
 // getallmanu
-app.get('/manu', async (req, res) => {
+getAllMenu.get('/manu', async (req, res) => {
   try {
     const database = client.db('cafeboardgame');
     const collection = database.collection('Menu');
@@ -58,7 +50,8 @@ app.get('/manu', async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-app.get('/boardgame', async (req, res) => {
+
+getAllBoardgame.get('/boardgame', async (req, res) => {
   try {
     const database = client.db('cafeboardgame');
     const collection = database.collection('boardgame');
