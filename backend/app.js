d30d27a5071a66d90fa9148686f563256beec11e
supakorn542 <supakorn542@gmail.com>
2024-09-05 17:21:15 +0700

    ryu

--- backend/app.js
+++ backend/app.js
@@ -1,92 +1,32 @@
 const dotenv = require("dotenv");
-const cors = require("cors")
-const fs = require('fs');
-const expressFunction = require('express');
-const mongoose = require('mongoose');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const morgan = require("morgan");
+const fs = require("fs");
 
-dotenv.config()
-const { ATLAS_URL } = process.env;
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
 
-const client = new MongoClient(ATLAS_URL, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const authRoutes = require('./routes/auth');
+const boardgameRoutes = require("./routes/boardgame");
+const menuRoutes = require("./routes/menurouters");
 
-// Create an Express app
-const app = expressFunction();
 
-// Connect to MongoDB
-async function connectToMongoDB() {
+dotenv.config();
+const mongoURI = process.env.MONGO_URI;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+const PORT = 3000;
+
+app.listen(PORT, async () => {
   try {
-    await client.connect();
-    app.use(morgan("dev"))
-    app.use(cors())
-    app.use(expressFunction.json());
-    console.log("Connected to MongoDB!");
-    
-    
-    fs.readdirSync('./routes')
-    .filter((file) => file.endsWith('.js')) 
-    .map((r) => {
-      console.log(`Loading route file: ${r}`);
-      return app.use('/', require('./routes/' + r)(client));
-    });
-  
-  
-
-  } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
+    await mongoose.connect(mongoURI);
+    console.log("Successfully connected to MongoDB");
+    console.log(`Server is running on port ${PORT}`);
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
   }
-}
-
-
-// Start the Express server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-  connectToMongoDB();
 });
-
-
-
-
-
-
-
-
-// getallmanu
-// getAllMenu.get('/manu', async (req, res) => {
-//   try {
-//     const database = client.db('cafeboardgame');
-//     const collection = database.collection('Menu');
-    
-//     // Fetch all documents from the students collection
-//     const Manu = await collection.find({}).toArray();
-    
-//     // Send the retrieved documents as a response
-//     res.status(200).json(Manu);
-//   } catch (error) {
-//     console.error("Error fetching Manu:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-// getAllBoardgame.get('/boardgame', async (req, res) => {
-//   try {
-//     const database = client.db('cafeboardgame');
-//     const collection = database.collection('boardgame');
-    
-//     // Fetch all documents from the students collection
-//     const item = await collection.find({}).toArray();
-    
-//     // Send the retrieved documents as a response
-//     res.status(200).json(item);
-//   } catch (error) {
-//     console.error("Error fetching Boardgame:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
