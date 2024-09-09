1c9297c3fedb876907d66c5f024309613f46f25a
supakorn542 <supakorn542@gmail.com>
2024-09-09 17:40:10 +0700

    log out แล้ว refresh หน้า

--- cafeboardgame/src/app/components/profile/profile.component.ts
+++ cafeboardgame/src/app/components/profile/profile.component.ts
@@ -8,13 +8,36 @@ import { catchError, of } from 'rxjs';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent  {
+export class ProfileComponent implements OnInit {
   userData: any; // ตัวแปรเก็บข้อมูลผู้ใช้
-  isLoading = true; // ใช้ในการบ่งชี้ว่ากำลังโหลดข้อมูล
-  error: string | null = null; // เก็บข้อความข้อผิดพลาด
+
+  userId: string | null = null;
 
   constructor(private authService: AuthService, private router: Router) {}
 
+  ngOnInit() {
+    this.authService.getUserId().subscribe((id) => {
+      this.userId = id;
+      // ทำสิ่งที่คุณต้องการกับ userId
+
+      //--------------//
+      if (this.userId) {
+        this.loadUserData(this.userId); // เรียกใช้ getUserById เพื่อดึงข้อมูลผู้ใช้
+      }
+      //--------------//
+    });
+  }
+
+  loadUserData(userId: string) {
+    this.authService.getUserById(userId).subscribe(
+      (data) => {
+        this.userData = data; // เก็บข้อมูลผู้ใช้ที่ได้รับ
+      },
+      (error) => {
+        console.error('Error fetching user data:', error); // ดีบักข้อผิดพลาด
+      }
+    );
+  }
 }
