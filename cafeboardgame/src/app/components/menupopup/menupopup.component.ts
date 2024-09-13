ec9c16bee671f35a8107b38d5bcabf3e6e1223fb
mumu3007 <guide.parangul@gmail.com>
2024-09-13 21:58:01 +0700

    ทำหน้า UI menupopup สมบูรณ์ - close #8

--- cafeboardgame/src/app/components/menupopup/menupopup.component.ts
+++ cafeboardgame/src/app/components/menupopup/menupopup.component.ts
@@ -12,6 +12,13 @@ export class MenupopupComponent implements OnInit {
   menuDetails: any;
   quantity: number = 1;
   totalPrice: number = 0;
+  sizes = ["tall", "grande", "venti"];
+  selectedSize?: string;
+  sizeImages = [
+    '../../../assets/tall.png',
+    '../../../assets/grande.png',
+    '../../../assets/venti.png'
+  ];
 
   constructor(private menupopupService: MenupopupService) { }
 
@@ -28,14 +35,48 @@ export class MenupopupComponent implements OnInit {
     });
   }
 
+  // ฟังก์ชันเพื่อเลือกภาพที่ตรงกับขนาด
+  getImageSource(index: any): string {
+    return this.sizeImages[index] || 'default-image.png'; // กรณีไม่มีภาพให้ใช้ default
+  }
+
+  getPrice(size: string): number {
+    switch (size) {
+      case 'tall': return this.menuDetails?.s_price || 0;
+      case 'grande': return this.menuDetails?.m_price || 0;
+      case 'venti': return this.menuDetails?.l_price || 0;
+      default: return 0;
+    }
+  }
+
+  getOz(size: string): string {
+    switch (size) {
+      case 'tall': return "(16oz)";
+      case 'grande': return "(24oz)";
+      case 'venti': return "(32oz)";
+      default: return "";
+    }
+  }
+
+  updateTotalPrice() {
+    // คำนวณ totalPrice จากขนาดที่เลือกและ quantity
+    this.totalPrice = this.getPrice(this.selectedSize!) * this.quantity;
+  }
+
+  onSizeChange(size: string) {
+    this.selectedSize = size; // อัปเดตขนาดที่เลือก
+    this.updateTotalPrice(); // คำนวณราคาใหม่
+  }
 
   increaseQuantity() {
     this.quantity++;
+    this.updateTotalPrice(); // คำนวณราคาใหม่
   }
 
   decreaseQuantity() {
     if (this.quantity > 1) {
       this.quantity--;
+      this.updateTotalPrice(); // คำนวณราคาใหม่
     }
   }
 
